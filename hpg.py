eb11823473395e2bc1c35ce928a2e651d82e2625
Nick Pascucci <npascut1@gmail.com>
2012-09-29 17:54:25 -0400

    Added ability to copy to X clipboard and to filter chars.

--- hpg.py
+++ hpg.py
@@ -13,24 +13,29 @@ import os
 import os.path
 import string
 import sys
+import subprocess
 
 __author__ = "Nick Pascucci (npascut1@gmail.com)"
 
-HELP_TEXT = ("Usage: hpg <identifier> [-l<password length>]" 
-             " [-e<excluded chars>]")
+HELP_TEXT = ("Usage: hpg [-l<password length>] [-e<excluded chars>] " 
+             "[-a] [-c] [-n] [-l<password length>]")
 CONFIG_DIR = os.path.expanduser("~/.hpg")
 KEYS_FILE = CONFIG_DIR + "/keys"
 
 def main():
+    BASE_CHARS = string.printable
     UNGOOD_CHARS = string.whitespace
     NUM_CHARS = 12
+    copy_to_clipboard = False
+    prompt_for_save = True
 
     # Check arguments.
     if len(sys.argv) < 2:
         print HELP_TEXT
         exit(1)
     if len(sys.argv) > 2:
-        for arg in sys.argv[2:]:
+        for arg in sys.argv[1:-1]:
+            arg = arg.strip()
             if arg.startswith("-l"):
                 try:
                     NUM_CHARS = int(arg[2:])
@@ -40,15 +45,26 @@ def main():
             elif arg.startswith("-e"):
                 print "Excluding", arg[2:]
                 UNGOOD_CHARS += arg[2:]
+            elif arg == "-a":
+                print "Using only alphanumeric characters."
+                BASE_CHARS = string.letters + string.digits
+            elif arg == "-c":
+                if not check_for_xsel():
+                    print ("xsel is not in your PATH. You must install it "
+                           "before passwords can be copied to the clipboard.")
+                    exit(1)
+                copy_to_clipboard = True
+            elif arg == "-n":
+                prompt_for_save = False
             else:
                 print "Unrecognized option %s. Ignoring." % arg
 
-    identifier = sys.argv[1]
+    identifier = sys.argv[-1]
     
-    store_key(identifier)
+    store_key(identifier, prompt_for_save)
 
     # Get password from user without echoing.
-    password = getpass.getpass("Password: ")
+    password = getpass.getpass("Salt: ")
     # Hash their password; this is our seed.
     pw_hash = hashlib.sha512(password).digest()
     generated_pass = hashlib.sha512(pw_hash + identifier).digest()
@@ -58,16 +74,20 @@ def main():
     position = 0
     while len(printable_pass) < NUM_CHARS: # and position < len(generated_pass):
         # Skip non-printable characters.
-        while (generated_pass[position] not in string.printable
+        while (generated_pass[position] not in BASE_CHARS
                or generated_pass[position] in UNGOOD_CHARS):
             position += 1
         # When the next printable character is encountered, add it.
         printable_pass += generated_pass[position]
         position += 1
 
-    print printable_pass
+    if copy_to_clipboard:
+        print "Password copied to X clipboard."
+        save_to_clipboard(printable_pass)
+    else:
+        print printable_pass
 
-def store_key(identifier):
+def store_key(identifier, prompt=True, save=False):
     # Check to see that the config dir exists, creating it if needed
     if not os.path.isdir(CONFIG_DIR):
         os.mkdir(CONFIG_DIR)
@@ -83,10 +103,25 @@ def store_key(identifier):
                 key_in_file = True
 
     if not key_in_file:
-        confirmation = raw_input("Store this key? [y/N] ")
-        if confirmation.lower() == "y":
+        if prompt and not save:
+            confirmation = raw_input("Store this key? [y/N] ")
+            if confirmation.lower() == "y":
+                save = True
+        if not save:
             keyfile = open(KEYS_FILE, "a")
             keyfile.write(identifier + "\n")
 
+def check_for_xsel():
+    which_proc = subprocess.Popen(['which', 'xsel'], 
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+    which_proc.wait()
+    return (which_proc.returncode == 0)
+
+def save_to_clipboard(password):
+    xsel_proc = subprocess.Popen(['xsel', '-pi'], stdin=subprocess.PIPE)
+    xsel_proc.communicate(password)
+
 if __name__ == "__main__":
     main()
+
